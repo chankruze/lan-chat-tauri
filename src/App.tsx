036660e897940c4efeadb42eaaeefb0f500ea6c1
Chandan Kumar Mandal <chankruze@gmail.com>
2025-07-22 17:45:04 +0530

    chore(style): setup tailwind css
    Signed-off-by: Chandan Kumar Mandal <chankruze@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
-
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+const App = () => {
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+    <main className="p-6 min-h-screen flex flex-col items-center justify-center space-y-4">
+      <h1 className="text-4xl font-bold text-red-600">LanChat</h1>
+      <p className="text-gray-600 font-mono">
+        A simple local chat application built with Tauri and React.
+      </p>
     </main>
   );
-}
+};
 
 export default App;
